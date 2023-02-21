eab924bbaf87b9f64bf0af106e6b304b1040989c
matthewcroughan <matt@croughan.sh>
2023-02-21 17:49:08 +0000

    flake: add substituters to nixConfig
    This caches the outputs of the Flake via Cachix

--- flake.nix
+++ flake.nix
@@ -1,5 +1,8 @@
 {
-  description = "A very basic flake";
+  nixConfig = {
+    extra-substituters = [ "https://ai.cachix.org" ];
+    extra-trusted-public-keys = [ "ai.cachix.org-1:N9dzRK+alWwoKXQlnn0H6aUx0lU/mspIoz8hMvGvbbc=" ];
+  };
 
   inputs = {
     nixpkgs = {

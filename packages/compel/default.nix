4394b44c93c0e3680e49abb4a7f3c49748f50db9
matthewcroughan <matt@croughan.sh>
2023-03-02 17:37:15 +0000

    invokeai: update to v2.3.1.post2
    Co-authored-by: Max <max@privatevoid.net>

---
+++ packages/compel/default.nix
@@ -0,0 +1,27 @@
+{ buildPythonPackage, fetchPypi, lib, setuptools, transformers, diffusers, torch }:
+
+buildPythonPackage rec {
+  pname = "compel";
+  version = "0.1.7";
+  format = "pyproject";
+
+  src = fetchPypi {
+    inherit pname version;
+    sha256 = "sha256-JP+PX0yENTNnfsAJ/hzgIA/cr/RhIWV1GEa1rYTdlnc=";
+  };
+
+  propagatedBuildInputs = [
+    setuptools
+    diffusers
+    transformers
+    torch
+  ];
+
+#  # TODO FIXME
+  doCheck = false;
+
+  meta = {
+    description = "A prompting enhancement library for transformers-type text embedding systems";
+    homepage = "https://github.com/damian0815/compel";
+  };
+}
---
+++ packages/safetensors/default.nix
@@ -0,0 +1,83 @@
+{ lib
+, fetchPypi
+, runCommand
+, rustPlatform
+, buildPythonPackage
+, setuptools
+, setuptools-rust
+, wheel
+, black
+, click
+, huggingface-hub
+, isort
+, jax
+, numpy
+, pytest
+, pytest-benchmark
+, tensorflow
+, torch
+}:
+
+let
+
+  pname = "safetensors";
+  version = "0.2.8";
+
+  patchedSrc = runCommand "patched-src" {
+    src = fetchPypi {
+      inherit pname version;
+      hash = "sha256-JyCyCmo4x5ncp5vXbK7qwvffWFqdT31Z+n4o7/nMsn8=";
+    };
+  } ''
+    unpackPhase
+    cp ${./Cargo.lock} $sourceRoot/Cargo.lock
+    cp -r $sourceRoot $out
+  '';
+in
+
+buildPythonPackage {
+  inherit pname version;
+  format = "pyproject";
+  src = patchedSrc;
+
+  cargoDeps = rustPlatform.fetchCargoTarball {
+    src = patchedSrc;
+    name = "${pname}-${version}";
+    hash = "sha256-IZKaw4NquK/BbIv1xkMFgNR20vve4H6Re76mvxtcNUA=";
+  };
+
+  nativeBuildInputs = [
+    setuptools
+    setuptools-rust
+    wheel
+    rustPlatform.cargoSetupHook
+    rustPlatform.rust.cargo
+    rustPlatform.rust.rustc
+  ];
+
+  propagatedBuildInputs = [
+    black
+    click
+#    flake
+#    flax
+#    h
+    huggingface-hub
+    isort
+    jax
+    numpy
+    pytest
+    pytest-benchmark
+    setuptools-rust
+    tensorflow
+    torch
+  ];
+
+  pythonImportsCheck = [ "safetensors" ];
+
+  meta = with lib; {
+    description = "Fast and Safe Tensor serialization";
+    homepage = "https://pypi.org/project/safetensors/";
+    license = with licenses; [ ];
+    maintainers = with maintainers; [ ];
+  };
+}

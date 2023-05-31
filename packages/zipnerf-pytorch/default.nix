d67a805cf1d8339457ffd655c9129a1b1b4b00d6
lucasew <lucas59356@gmail.com>
2023-05-31 00:10:36 -0300

    checkpoint
    Signed-off-by: lucasew <lucas59356@gmail.com>

--- packages/zipnerf-pytorch/default.nix
+++ packages/zipnerf-pytorch/default.nix
@@ -16,6 +16,15 @@
 , makeWrapper
 , accelerate
 # , nvidia-thrust
+, numpy
+, absl-py
+, gin-config
+, matplotlib
+, trimesh
+, opencv3
+, scipy
+, scikitimage
+, ninja
 }:
 
 let
@@ -55,10 +64,16 @@ let
       ]
       else [ cudaPackages.cudatoolkit ]);
 
-    nativeBuildInputs = [ which ]
+    nativeBuildInputs = [ which ninja ]
     ++ (if isRocm
       then [ hip ]
       else [ cudaPackages.cuda_nvcc ]);
+
+    postInstall = ''
+      ls -R $out
+    '';
+
+    pythonImportsCheck = [ "_gridencoder" ];
 
   }).overrideDerivation (old: {
     stdenv = if isRocm then stdenv else  gcc11Stdenv;
@@ -66,19 +81,30 @@ let
 
 in
 
-buildPythonPackage {
-  pname = "zipnerf";
+buildPythonPackage rec {
+  pname = "zipnerf_pytorch";
   inherit version;
 
   format = "other";
 
   inherit src;
 
-  buildInputs = [ gridencoder ];
+  _INSTALL_PATH = "${python.sitePackages}/zipnerf_pytorch";
+
+  prePatch = ''
+    substituteInPlace internal/configs.py \
+      --replace "gin.add_config_file_search_path('configs/')" "gin.add_config_file_search_path('$out/$_INSTALL_PATH/configs/')"
+    substituteInPlace $(find -type f | grep .py) \
+      --replace 'from internal' 'from zipnerf_pytorch.internal'
+  '';
+
+  buildInputs = [
+    gridencoder
+  ];
 
   nativeBuildInputs = [ wrapPython makeWrapper which ];
 
-  wrappedPython = (python.withPackages (p: with p; [
+  propagatedBuildInputs = [
     numpy
     absl-py
     accelerate
@@ -88,19 +114,29 @@ buildPythonPackage {
     opencv3
     scipy
     scikitimage
-  ])).interpreter;
+  ];
+
+  pythonImportsCheck = [ pname ];
 
   installPhase = ''
-    export INSTALL_DIR=$out/${python.sitePackages}/zipnerf-pytorch
+    export INSTALL_DIR=$out/$_INSTALL_PATH
     mkdir -p $INSTALL_DIR
     cp -r * $INSTALL_DIR
+
+    buildPythonPath
+    echo pythonpath
+    echo $program_PYTHONPATH
+
+    touch $INSTALL_DIR/__init__.py
 
     mkdir -p $out/bin
 
     for bin in eval extract render train; do
-      makeWrapper $wrappedPython $out/bin/zipnerf-$bin \
-        --prefix PYTHONPATH : $INSTALL_DIR \
-        --add-flags $INSTALL_DIR/$bin.py
+      makeWrapper ${(python.withPackages (p: propagatedBuildInputs)).interpreter} $out/bin/zipnerf_$bin \
+        --prefix PYTHONPATH : $out/${python.sitePackages} \
+        --add-flags $INSTALL_DIR/$bin.py \
+        --prefix PYTHONPATH : "$program_PYTHONPATH" \
+        --prefix PATH : "$program_PATH"
     done
   '';
 }

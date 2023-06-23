e21163af46d87a51a3d2f3b37f19bc10cffaef2b
Max <max@privatevoid.net>
2023-06-23 20:39:10 +0200

    overlays/python: build rocm torch from source

--- modules/nixpkgs-config/default.nix
+++ modules/nixpkgs-config/default.nix
@@ -6,10 +6,18 @@
       inherit system;
       config = {
         allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
+          # TODO: classify
           "torch"
           "triton"
           "cuda_nvtx"
           "cuda_cudart"
+
+          # for ROCm?
+          "libcublas"
+          "libcusparse"
+          "cuda_nvcc"
+          "cuda_nvprof"
+          "cuda_cupti"
         ];
       };
     };
--- overlays/python/default.nix
+++ overlays/python/default.nix
@@ -38,31 +38,12 @@ lib: {
     opencv-python = final.opencv4;
   };
 
-  torchRocm = final: prev: rec {
-    # TODO: figure out how to patch torch-bin trying to access /opt/amdgpu
-    # there might be an environment variable for it, can use a wrapper for that
-    # otherwise just grep the world for /opt/amdgpu or something and substituteInPlace the path
-    # you can run this thing without the fix by creating /opt and running nix build nixpkgs#libdrm --inputs-from . --out-link /opt/amdgpu
-    torch-bin = prev.torch-bin.overrideAttrs (old: {
-      src = final.pkgs.fetchurl {
-        name = "torch-1.13.1+rocm5.1.1-cp310-cp310-linux_x86_64.whl";
-        url = "https://download.pytorch.org/whl/rocm5.1.1/torch-1.13.1%2Brocm5.1.1-cp310-cp310-linux_x86_64.whl";
-        hash = "sha256-qUwAL3L9ODy9hjne8jZQRoG4BxvXXLT7cAy9RbM837A=";
-      };
-      postFixup = (old.postFixup or "") + ''
-        ${final.pkgs.gnused}/bin/sed -i s,/opt/amdgpu/share/libdrm/amdgpu.ids,/tmp/nix-pytorch-rocm___/amdgpu.ids,g $out/${final.python.sitePackages}/torch/lib/libdrm_amdgpu.so
-      '';
+  torchRocm = final: prev: {
+    torch = prev.torch.override {
+      magma = final.pkgs.magma-cuda;
+      cudaSupport = false;
       rocmSupport = true;
-    });
-    torchvision-bin = prev.torchvision-bin.overrideAttrs (old: {
-      src = final.pkgs.fetchurl {
-        name = "torchvision-0.14.1+rocm5.1.1-cp310-cp310-linux_x86_64.whl";
-        url = "https://download.pytorch.org/whl/rocm5.1.1/torchvision-0.14.1%2Brocm5.1.1-cp310-cp310-linux_x86_64.whl";
-        hash = "sha256-8CM1QZ9cZfexa+HWhG4SfA/PTGB2475dxoOtGZ3Wa2E=";
-      };
-    });
-    torch = torch-bin;
-    torchvision = torchvision-bin;
+    };
   };
 
   torchCuda = final: prev: {

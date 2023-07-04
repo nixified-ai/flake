0297d02b6fb2f9dd6eb139786527016b3791cf0f
qwbarch <qwbarch@gmail.com>
2023-07-04 04:06:23 -0400

    Add invokeai-configure (doesn't work)

--- projects/invokeai/package.nix
+++ projects/invokeai/package.nix
@@ -1,8 +1,8 @@
 { aipython3
-# misc
+  # misc
 , lib
 , src
-# extra deps
+  # extra deps
 , libdrm
 }:
 
@@ -69,46 +69,56 @@ aipython3.buildPythonPackage {
   pythonRemoveDeps = [ "clip" "pyreadline3" "flaskwebgui" "opencv-python" ];
   pythonRelaxDeps = [ "dnspython" "protobuf" "flask" "flask-socketio" "pytorch-lightning" "torch" "transformers" ];
   makeWrapperArgs = [
-    '' --run '
-      if [ -d "/usr/lib/wsl/lib" ]
-      then
-          echo "Running via WSL (Windows Subsystem for Linux), setting LD_LIBRARY_PATH=/usr/lib/wsl/lib"
-          set -x
-          export LD_LIBRARY_PATH="/usr/lib/wsl/lib"
-          set +x
-      fi
-      '
+    ''
+      --run '
+         if [ ! -d "${builtins.getEnv "HOME"}/invokeai" ]
+         then
+           echo "Running invokeai-configure"
+           set -x
+           invokeai-configure
+           set +x
+         fi
+         '
+      --run '
+          if [ -d "/usr/lib/wsl/lib" ]
+          then
+              echo "Running via WSL (Windows Subsystem for Linux), setting LD_LIBRARY_PATH=/usr/lib/wsl/lib"
+              set -x
+              export LD_LIBRARY_PATH="/usr/lib/wsl/lib"
+              set +x
+          fi
+          '
     ''
   ] ++ lib.optionals (aipython3.torch.rocmSupport or false) [
     '' --run '
-      if [ ! -e /tmp/nix-pytorch-rocm___/amdgpu.ids ]
-      then
-          mkdir -p /tmp/nix-pytorch-rocm___
-          ln -s ${libdrm}/share/libdrm/amdgpu.ids /tmp/nix-pytorch-rocm___/amdgpu.ids
-      fi
-      '
+          if [ ! -e /tmp/nix-pytorch-rocm___/amdgpu.ids ]
+          then
+              mkdir -p /tmp/nix-pytorch-rocm___
+              ln -s ${libdrm}/share/libdrm/amdgpu.ids /tmp/nix-pytorch-rocm___/amdgpu.ids
+          fi
+          '
     ''
     # See note about consumer GPUs:
     # https://docs.amd.com/bundle/ROCm-Deep-Learning-Guide-v5.4.3/page/Troubleshooting.html
     " --set-default HSA_OVERRIDE_GFX_VERSION 10.3.0"
   ];
   patchPhase = ''
-    runHook prePatch
+        runHook prePatch
 
-    # Add subprocess to the imports
-    substituteInPlace ./ldm/invoke/config/invokeai_configure.py --replace \
-        'import shutil' \
-'
-import shutil
-import subprocess
-'
-    # shutil.copytree will inherit the permissions of files in the /nix/store
-    # which are read only, so we subprocess.call cp instead and tell it not to
-    # preserve the mode
-    substituteInPlace ./ldm/invoke/config/invokeai_configure.py --replace \
-      "shutil.copytree(configs_src, configs_dest, dirs_exist_ok=True)" \
-      "subprocess.call('cp -r --no-preserve=mode {configs_src} {configs_dest}'.format(configs_src=configs_src, configs_dest=configs_dest), shell=True)"
-    runHook postPatch
+        # Add subprocess to the imports
+        substituteInPlace ./ldm/invoke/config/invokeai_configure.py --replace \
+            'import shutil' \
+    '
+    import shutil
+    import subprocess
+    '
+        # shutil.copytree will inherit the permissions of files in the /nix/store
+        # which are read only, so we subprocess.call cp instead and tell it not to
+        # preserve the mode
+        substituteInPlace ./ldm/invoke/config/invokeai_configure.py --replace \
+          "shutil.copytree(configs_src, configs_dest, dirs_exist_ok=True)" \
+          "subprocess.call('cp -r --no-preserve=mode {configs_src} {configs_dest}'.format(configs_src=configs_src, configs_dest=configs_dest), shell=True)"
+        runHook postPatch
   '';
   postFixup = ''
     chmod +x $out/bin/*

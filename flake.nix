558012a26792bcf8cda485852d46349a3501a040
matthewcroughan <matt@croughan.sh>
2023-11-02 19:09:09 +0000

    wip

--- flake.nix
+++ flake.nix
@@ -14,7 +14,12 @@
         "x86_64-linux"
         "aarch64-linux"
       ];
-      perSystem = { config, pkgs, system, ... }: {
+      perSystem = { config, pkgs, system, ... }:
+        let
+          genOverridenDrvList = drv: howMany: builtins.genList (x: drv.overrideAttrs { name = drv.name + "-" + toString x; }) howMany;
+          genOverridenDrvLinkFarm = drv: howMany: pkgs.linkFarm (drv.name + "-linkfarm-${toString howMany}") (builtins.genList (x: rec { name = drv.name + "-" + toString x; path = drv.overrideAttrs { inherit name; }; }) howMany);
+        in
+      {
         _module.args.pkgs = import inputs.nixpkgs { overlays = [ inputs.self.overlays.default ]; inherit system; };
         overlayAttrs = config.legacyPackages;
         legacyPackages = {
@@ -28,38 +33,10 @@
           msDos622-image = config.legacyPackages.makeMsDos622Image {};
           win30-image = config.legacyPackages.makeWin30Image {};
           wfwg311-image = config.legacyPackages.makeWfwg311Image {};
-          macOS-30 = pkgs.linkFarm "macOS-Fuck" [
-            { name = "macos1"; path = macOS-ventura-image.overrideAttrs { name = "macos1"; }; }
-            { name = "macos2"; path = macOS-ventura-image.overrideAttrs { name = "macos2"; }; }
-            { name = "macos3"; path = macOS-ventura-image.overrideAttrs { name = "macos3"; }; }
-            { name = "macos4"; path = macOS-ventura-image.overrideAttrs { name = "macos4"; }; }
-            { name = "macos5"; path = macOS-ventura-image.overrideAttrs { name = "macos5"; }; }
-            { name = "macos6"; path = macOS-ventura-image.overrideAttrs { name = "macos6"; }; }
-            { name = "macos7"; path = macOS-ventura-image.overrideAttrs { name = "macos7"; }; }
-            { name = "macos8"; path = macOS-ventura-image.overrideAttrs { name = "macos8"; }; }
-            { name = "macos9"; path = macOS-ventura-image.overrideAttrs { name = "macos9"; }; }
-            { name = "macos10"; path = macOS-ventura-image.overrideAttrs { name = "macos10"; }; }
-            { name = "macos11"; path = macOS-ventura-image.overrideAttrs { name = "macos11"; }; }
-            { name = "macos12"; path = macOS-ventura-image.overrideAttrs { name = "macos12"; }; }
-            { name = "macos13"; path = macOS-ventura-image.overrideAttrs { name = "macos13"; }; }
-            { name = "macos14"; path = macOS-ventura-image.overrideAttrs { name = "macos14"; }; }
-            { name = "macos15"; path = macOS-ventura-image.overrideAttrs { name = "macos15"; }; }
-            { name = "macos16"; path = macOS-ventura-image.overrideAttrs { name = "macos16"; }; }
-            { name = "macos17"; path = macOS-ventura-image.overrideAttrs { name = "macos17"; }; }
-            { name = "macos18"; path = macOS-ventura-image.overrideAttrs { name = "macos18"; }; }
-            { name = "macos19"; path = macOS-ventura-image.overrideAttrs { name = "macos19"; }; }
-            { name = "macos20"; path = macOS-ventura-image.overrideAttrs { name = "macos20"; }; }
-            { name = "macos21"; path = macOS-ventura-image.overrideAttrs { name = "macos21"; }; }
-            { name = "macos22"; path = macOS-ventura-image.overrideAttrs { name = "macos22"; }; }
-            { name = "macos23"; path = macOS-ventura-image.overrideAttrs { name = "macos23"; }; }
-            { name = "macos24"; path = macOS-ventura-image.overrideAttrs { name = "macos24"; }; }
-            { name = "macos25"; path = macOS-ventura-image.overrideAttrs { name = "macos25"; }; }
-            { name = "macos26"; path = macOS-ventura-image.overrideAttrs { name = "macos26"; }; }
-            { name = "macos27"; path = macOS-ventura-image.overrideAttrs { name = "macos27"; }; }
-            { name = "macos28"; path = macOS-ventura-image.overrideAttrs { name = "macos28"; }; }
-            { name = "macos29"; path = macOS-ventura-image.overrideAttrs { name = "macos29"; }; }
-            { name = "macos30"; path = macOS-ventura-image.overrideAttrs { name = "macos30"; }; }
-          ];
+          macOS-repeatability-test = genOverridenDrvLinkFarm macOS-ventura-image 10;
+          wfwg311-repeatability-test = genOverridenDrvLinkFarm wfwg311-image 1000;
+          win30-repeatability-test = genOverridenDrvLinkFarm win30-image 1000;
+          msDos622-repeatability-test = genOverridenDrvLinkFarm msDos622-image 1000;
         };
       };
     };

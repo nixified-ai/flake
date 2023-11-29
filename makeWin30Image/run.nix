25efa2d4a199903e379c0004ce28a0a579475f88
matthewcroughan <matt@croughan.sh>
2023-11-29 02:00:29 +0000

    wip dosbox

--- makeWin30Image/run.nix
+++ makeWin30Image/run.nix
@@ -9,8 +9,8 @@
 let
   dosboxConf = writeText "dosbox.conf" ''
     [autoexec]
-    imgmount c win30.img -size 512,63,16,507 -t hdd -fs fat
-    boot -l c
+    imgmount C win30.img -size 512,63,16,507 -t hdd -fs fat
+    boot -l C
   '';
 in
 writeShellScriptBin "run-win30.sh" ''

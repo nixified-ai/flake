e0d8a1d5009ec92d2d6a8e06aee1570cdb7784cf
matthewcroughan <matt@croughan.sh>
2023-12-03 21:19:08 +0000

    effects: wip

--- flake.nix
+++ flake.nix
@@ -55,14 +55,14 @@
               do
                 set +e
                 echo 'Running Nix'
-                if ! nix build --option build-users-group "" --store local?read-only=true github:matthewcroughan/nixtheplanet#macos-ventura-image --keep-failed -L
+                if ! nix build --option build-users-group "" --store 'local?read-only=true' --timeout 5000 github:matthewcroughan/nixtheplanet#macos-ventura-image --keep-failed -L
                 then
                   upload_failure
                   echo NixThePlanet: iteration "$iteration" failed
                   exit 1
                 fi
                 echo 'Running Nix'
-                if ! nix build --option build-users-group "" --store local?read-only=true --timeout 5000 github:matthewcroughan/nixtheplanet#macos-ventura-image --rebuild --keep-failed -L
+                if ! nix build --option build-users-group "" --store 'local?read-only=true' --timeout 5000 github:matthewcroughan/nixtheplanet#macos-ventura-image --rebuild --keep-failed -L
                 then
                   upload_failure
                   echo NixThePlanet: iteration "$iteration" failed

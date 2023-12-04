640aeed4b168c4663c2c891e47e7932477db08ea
matthewcroughan <matt@croughan.sh>
2023-12-04 01:42:06 +0000

    effects: desparateush

--- flake.nix
+++ flake.nix
@@ -63,7 +63,7 @@
                 set +e
                 echo 'Running Nix'
                 nix show-config | grep trusted
-                if ! nix build --timeout 5000 ${inputs.self}#macos-ventura-image --keep-failed -L
+                if ! nix build --option build-users-group "" --timeout 5000 ${inputs.self}#macos-ventura-image --keep-failed -L
                 then
                   upload_failure
                   echo NixThePlanet: iteration "$iteration" failed

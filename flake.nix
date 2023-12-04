74b0aa3c398507bbf52e27c22b1ba90bad50c13e
matthewcroughan <matt@croughan.sh>
2023-12-04 00:39:05 +0000

    effects: desparate

--- flake.nix
+++ flake.nix
@@ -41,6 +41,7 @@
     export NIX_CONF_DIR=$ROOT/etc
 
               unset NIX_REMOTE
+              unset NIX_STORE
 #              mkdir $TMPDIR nixtheplanet-test-logs
               nix-store --load-db < ${closure}/registration
 
@@ -70,14 +71,14 @@
               do
                 set +e
                 echo 'Running Nix'
-                if ! nix build --option build-users-group "" --store ./foo --timeout 5000 github:matthewcroughan/nixtheplanet#macos-ventura-image --keep-failed -L
+                if ! nix build --option build-users-group "" --store "$ROOT" --timeout 5000 github:matthewcroughan/nixtheplanet#macos-ventura-image --keep-failed -L
                 then
                   upload_failure
                   echo NixThePlanet: iteration "$iteration" failed
                   exit 1
                 fi
                 echo 'Running Nix'
-                if ! nix build --option build-users-group "" --store ./foo --timeout 5000 github:matthewcroughan/nixtheplanet#macos-ventura-image --rebuild --keep-failed -L
+                if ! nix build --option build-users-group "" --store "$ROOT" --timeout 5000 github:matthewcroughan/nixtheplanet#macos-ventura-image --rebuild --keep-failed -L
                 then
                   upload_failure
                   echo NixThePlanet: iteration "$iteration" failed

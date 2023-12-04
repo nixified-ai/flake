d1cccc576df68722b95aa8aea51b9913c21343ef
matthewcroughan <matt@croughan.sh>
2023-12-04 00:21:06 +0000

    effects: desparate

--- flake.nix
+++ flake.nix
@@ -39,6 +39,7 @@
     export NIX_LOG_DIR=$ROOT/var/log/nix
     export NIX_STATE_DIR=$ROOT/var/nix
     export NIX_CONF_DIR=$ROOT/etc
+    export NIX_REMOTE="local"
 
 #              mkdir $TMPDIR nixtheplanet-test-logs
               nix-store --load-db < ${closure}/registration

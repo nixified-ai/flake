2dcb03589e42a63ac08e135ed5cd51be6db2582c
Adrian Gierakowski <agierakowski@gmail.com>
2025-10-18 11:21:47 +0200

    comfy: use npins to track comfyui-unwrapped, frontend, docs and templates

--- flake-modules/projects/comfyui/pkgs/comfyui-frontend-package/package.nix
+++ flake-modules/projects/comfyui/pkgs/comfyui-frontend-package/package.nix
@@ -1,2 +1,32 @@
-{ python3Packages }:
-python3Packages.callPackage ../../../../packages/comfyui-frontend-package/default.nix { }
+{
+  python3Packages,
+  comfyuiNpins,
+}:
+let
+  npin = comfyuiNpins.comfyui-frontend-package;
+in
+python3Packages.callPackage (
+  {
+    buildPythonPackage,
+    fetchurl,
+  }:
+  buildPythonPackage rec {
+    pname = "comfyui_frontend_package";
+
+    inherit (npin) version;
+    src = fetchurl {
+      inherit (npin) url;
+      sha256 = npin.hash;
+    };
+
+    format = "setuptools";
+    pythonImportsCheck = [
+      pname
+    ];
+
+    meta = {
+      description = "comfyui-frontend-package";
+      homepage = "https://github.com/Comfy-Org/ComfyUI_frontend";
+    };
+  }
+) { }

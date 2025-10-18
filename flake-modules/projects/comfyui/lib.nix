d6a7299325dde437a4d5d228ba65a0f38659876d
Adrian Gierakowski <agierakowski@gmail.com>
2025-10-18 09:25:51 +0200

    mkComfyUICustomNode: allow use of finalAddrs pattern
    args can now be a derivation or a function: `finalAttrs: overrides`

--- flake-modules/projects/comfyui/lib.nix
+++ flake-modules/projects/comfyui/lib.nix
@@ -5,23 +5,27 @@
 {
   mkComfyUICustomNode =
     let
-      install = name: ''
-        mkdir -p $out/${python3Packages.python.sitePackages}/custom_nodes/${name}
-        shopt -s dotglob
-        shopt -s extglob
-        cp -r ./!($out|$src) $out/${python3Packages.python.sitePackages}/custom_nodes/${name}
-      '';
+      install =
+        name:
+        let
+          installDir = "$out/${python3Packages.python.sitePackages}/custom_nodes/${name}";
+        in
+        ''
+          mkdir -p ${installDir}
+          shopt -s dotglob
+          shopt -s extglob
+          cp -r ./!($out|$src) ${installDir}
+        '';
     in
     args:
-    stdenv.mkDerivation (
-      {
+    (if args ? "overrideAttrs" then args else stdenv.mkDerivation args).overrideAttrs (
+      finalAttrs: previousAttrs: {
         installPhase = ''
           runHook preInstall
           mkdir -p $out/
-          ${install args.name or args.pname}
+          ${install finalAttrs.name or finalAttrs.pname}
           runHook postInstall
         '';
       }
-      // args
     );
 }

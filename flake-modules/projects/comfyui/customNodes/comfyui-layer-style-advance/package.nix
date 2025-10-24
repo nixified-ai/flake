6f502f7bb0ea66402bcc9751a7962397e29dd8c3
Adrian Gierakowski <agierakowski@gmail.com>
2025-10-25 00:01:02 +0200

    bitsandbytes: only include if cudaSupport || rocmSupport

--- flake-modules/projects/comfyui/customNodes/comfyui-layer-style-advance/package.nix
+++ flake-modules/projects/comfyui/customNodes/comfyui-layer-style-advance/package.nix
@@ -1,5 +1,6 @@
 {
   python3Packages,
+  config,
 }:
 let
   blend-modes = python3Packages.callPackage ../../../../packages/blend-modes/default.nix { };
@@ -20,45 +21,47 @@ in
 
   dontBuild = true;
 
-  propagatedBuildInputs = with python3Packages; [
-    matplotlib
-    scikit-image
-    scikit-learn
-    opencv4
-    pymatting
-    timm
-    blend-modes
-    transformers
-    diffusers
-    loguru
-    colour-science
-    huggingface-hub
-    segment-anything
-    addict
-    omegaconf
-    yapf
-    wget
-    iopath
-    mediapipe
-    typer-config
-    fastapi
-    rich
-    google-generativeai
-    google-genai
-    pillow
-    ultralytics
-    transparent-background
-    accelerate
-    onnxruntime
-    bitsandbytes
-    peft
-    protobuf
-    hydra-core
-    blind-watermark
-    qrcode
-    pyzbar
-    psd-tools
-    zhipuai
-    openai
-  ];
+  propagatedBuildInputs =
+    with python3Packages;
+    [
+      matplotlib
+      scikit-image
+      scikit-learn
+      opencv4
+      pymatting
+      timm
+      blend-modes
+      transformers
+      diffusers
+      loguru
+      colour-science
+      huggingface-hub
+      segment-anything
+      addict
+      omegaconf
+      yapf
+      wget
+      iopath
+      mediapipe
+      typer-config
+      fastapi
+      rich
+      google-generativeai
+      google-genai
+      pillow
+      ultralytics
+      transparent-background
+      accelerate
+      onnxruntime
+      peft
+      protobuf
+      hydra-core
+      blind-watermark
+      qrcode
+      pyzbar
+      psd-tools
+      zhipuai
+      openai
+    ]
+    ++ lib.optional (config.cudaSupport || config.rocmSupport) bitsandbytes;
 }

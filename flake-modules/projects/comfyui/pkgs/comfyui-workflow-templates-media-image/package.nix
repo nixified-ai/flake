8ea367f926ec19356bf206cbda0f2c578080d555
Andrew Marshall <andrew@johnandrewmarshall.com>
2026-02-21 21:44:02 -0500

    comfyui: Fix missing template packages
    Without these, comfyui fails with e.g.
    
    > Failed to resolve template asset paths: Media package
    > 'comfyui_workflow_templates_media_api' is not installed for bundle
    > 'media-api'
    
    and then no default templates are present in the UI. Adding these
    missing deps fixes.
    
    Basically just did `npin add pypi` and then copy-pasted the existing
    media-core pkg and did trivial rename.

---
+++ flake-modules/projects/comfyui/pkgs/comfyui-workflow-templates-media-image/package.nix
@@ -0,0 +1,40 @@
+{
+  python3Packages,
+  comfyuiNpins,
+}:
+let
+  npin = comfyuiNpins.comfyui-workflow-templates-media-image;
+in
+python3Packages.callPackage (
+  {
+    lib,
+    buildPythonPackage,
+    fetchurl,
+  }:
+  buildPythonPackage rec {
+    pname = "comfyui_workflow_templates_media_image";
+
+    inherit (npin) version;
+    src = fetchurl {
+      inherit (npin) url;
+      sha256 = npin.hash;
+    };
+
+    format = "pyproject";
+
+    nativeBuildInputs = with python3Packages; [
+      setuptools
+      wheel
+    ];
+
+    pythonImportsCheck = [
+      pname
+    ];
+
+    meta = with lib; {
+      description = "Media-Image module for ComfyUI workflow templates.";
+      homepage = "nix";
+      license = licenses.gpl3;
+    };
+  }
+) { }

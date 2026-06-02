1bc6187c9b17a1a4b59c9212ce6f504f9ffe8e86
Adrian Gierakowski <agierakowski@gmail.com>
2026-06-02 20:09:58 +0200

    add Suzie1/ComfyUI_Comfyroll_CustomNodes

---
+++ flake-modules/projects/comfyui/customNodes/comfyui-comfyroll-customnodes/package.nix
@@ -0,0 +1,8 @@
+{ }:
+{
+  postPatch = ''
+    substituteInPlace nodes/nodes_graphics_text.py \
+      --replace-fail 'font_dir = "/usr/share/fonts/truetype"' \
+      'font_dir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), "fonts")'
+  '';
+}

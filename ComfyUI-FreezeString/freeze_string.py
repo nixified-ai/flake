8ce4fbf8b0b786ce75a4493f95b2eea66aa37273
Adrian Gierakowski <agierakowski@gmail.com>
2026-06-21 08:35:18 +0200

    feat(comfyui): add disable toggle to FreezeStringNode with backward compatibility and tests

--- ComfyUI-FreezeString/freeze_string.py
+++ ComfyUI-FreezeString/freeze_string.py
@@ -8,6 +8,7 @@ class FreezeStringNode:
             },
             "optional": {
                 "text_input": ("STRING", {"forceInput": True, "lazy": True}),
+                "disable": ("BOOLEAN", {"default": False}),
             }
         }
 
@@ -17,14 +18,18 @@ class FreezeStringNode:
     CATEGORY = "utils"
     OUTPUT_NODE = True
 
-    def check_lazy_status(self, freeze, frozen_text, **kwargs):
-        if freeze:
+    def check_lazy_status(self, freeze, frozen_text, disable=False, **kwargs):
+        if disable or freeze:
             return []
         return ["text_input"]
 
-    def process(self, freeze, frozen_text, text_input=None):
-        if freeze:
+    def process(self, freeze, frozen_text, disable=False, text_input=None):
+        if disable:
+            chosen_value = None
+            ui_text = frozen_text
+        elif freeze:
             chosen_value = frozen_text
+            ui_text = str(chosen_value)
         else:
             chosen_value = text_input if text_input is not None else ""
 
@@ -33,6 +38,9 @@ class FreezeStringNode:
         # (e.g., using \n.join) before sending it to the "ui" payload so the frontend doesn't crash.
         if isinstance(chosen_value, list):
             ui_text = "\n".join(str(item) for item in chosen_value)
+        elif chosen_value is None:
+            # If disabled, chosen_value is None, but ui_text is already set to frozen_text
+            pass
         else:
             ui_text = str(chosen_value)
 
@@ -44,7 +52,9 @@ class FreezeStringNode:
         }
 
     @classmethod
-    def IS_CHANGED(cls, freeze, frozen_text, text_input=None):
+    def IS_CHANGED(cls, freeze, frozen_text, disable=False, text_input=None):
+        if disable:
+            return "disabled"
         if freeze:
             # If freeze is True, return the frozen_text string so ComfyUI caches it properly.
             return frozen_text

ece8d869c2bb394e032bd045cb09f17c84168213
Adrian Gierakowski <agierakowski@gmail.com>
2026-06-28 07:37:47 +0200

    vm test: restore CPU test and add GPU test under: comfyui-container-gpu

--- flake-modules/projects/comfyui/vm-test/default.nix
+++ flake-modules/projects/comfyui/vm-test/default.nix
@@ -1,73 +1,32 @@
 {
   testers,
   nixosModule,
-  lib,
-  python3,
-  writeShellScript,
+  callPackage,
 }:
+let
+  common = callPackage ./common.nix { };
+in
 testers.nixosTest {
   name = "comfyui";
-  containers.machine =
+  nodes.machine =
     { pkgs, ... }:
     {
       imports = [ nixosModule ];
-
-      systemd.services.comfyui.serviceConfig = {
-        PrivateUsers = lib.mkForce false;
-        ProtectSystem = lib.mkForce false;
-      };
-
-      hardware.graphics.enable = true;
-      services.xserver.videoDrivers = [ "nvidia" ];
-      hardware.nvidia.open = false;
-
-      virtualisation.systemd-nspawn.options = [
-        "--bind=/dev/nvidia0"
-        "--bind=/dev/nvidiactl"
-        "--bind=/dev/nvidia-uvm"
-        "--bind=/dev/nvidia-uvm-tools"
-      ];
-
-      services.comfyui = {
+      virtualisation.memorySize = 12288;
+      virtualisation.cores = 1;
+      zramSwap = {
         enable = true;
-        host = "::,0.0.0.0";
-        port = 8189;
-        extraFlags = [
-          "--deterministic"
-          "--fast"
-        ];
-        acceleration = "cuda";
-        customNodes = lib.filter (drv: drv.pname != "comfyui-rife-tensorrt-auto") (
-          lib.attrValues pkgs.comfyuiCustomNodes
-        );
-        models = with pkgs.nixified-ai.models; [
-          clip_vision_vit_h-upscaler
-          control-lora-rank128-v11p-sd15-canny-fp16
-          face-yolov8m
-          hyper-sd15-1step-lora
-          ip-adapter-plus-sd15
-          stable-diffusion-v1-5
-        ];
+        algorithm = "zstd";
+        memoryPercent = 100;
+      };
+      services.comfyui = (common.comfyuiConfig pkgs) // {
+        acceleration = false;
       };
     };
   testScript =
-    { containers, ... }:
-    let
-      imagePath1 = "${containers.machine.services.comfyui.home}/.local/share/comfyui/output/ComfyUI_00001.png";
-      imagePath2 = "${containers.machine.services.comfyui.home}/.local/share/comfyui/output/ComfyUI_00002_.png";
-      apiTest = writeShellScript "" ''
-        ${lib.getExe python3} ${./api.py} ${./custom-nodes-test.json} --port ${toString containers.machine.services.comfyui.port}
-      '';
-    in
-    ''
-      start_all()
-      machine.wait_for_unit("multi-user.target")
-      machine.wait_for_unit("comfyui.service")
-      machine.wait_for_open_port(${toString containers.machine.services.comfyui.port})
-      machine.succeed("${apiTest}")
-      machine.wait_for_file("${imagePath1}")
-      machine.wait_for_file("${imagePath2}")
-      machine.copy_from_vm("${imagePath1}")
-      machine.copy_from_vm("${imagePath2}")
-    '';
+    { nodes, ... }:
+    common.testScript {
+      home = nodes.machine.services.comfyui.home;
+      port = nodes.machine.services.comfyui.port;
+    };
 }

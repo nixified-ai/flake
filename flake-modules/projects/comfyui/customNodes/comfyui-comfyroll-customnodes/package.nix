fe5adc0621cd751aee56561bd3997ba428289494
Adrian Gierakowski <agierakowski@gmail.com>
2026-06-30 07:27:21 +0200

    fix(customNodes): resolve dependencies and syntax errors for multiple nodes

--- flake-modules/projects/comfyui/customNodes/comfyui-comfyroll-customnodes/package.nix
+++ flake-modules/projects/comfyui/customNodes/comfyui-comfyroll-customnodes/package.nix
@@ -6,6 +6,10 @@
     substituteInPlace nodes/nodes_graphics_text.py \
       --replace-fail 'font_dir = "/usr/share/fonts/truetype"' \
       'font_dir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), "fonts")'
+    substituteInPlace nodes/nodes_list.py nodes/nodes_xygrid.py \
+      --replace-warn 'C:\Windows\Fonts' 'C:\\Windows\\Fonts' \
+      --replace-warn 'fonts\Roboto-Regular.ttf' 'fonts\\Roboto-Regular.ttf' \
+      --replace-warn 'fonts\Consolas.ttf' 'fonts\\Consolas.ttf'
   '';
 
   propagatedBuildInputs = with python3Packages; [

95e18d58300cf105313a0417a8f4a8c3e08a83ef
Adrian Gierakowski <agierakowski@gmail.com>
2026-07-07 16:44:10 +0200

    fix llama.cpp integration

--- flake-modules/projects/comfyui/customNodes/comfyui-llm-text-processor/package.nix
+++ flake-modules/projects/comfyui/customNodes/comfyui-llm-text-processor/package.nix
@@ -1,4 +1,8 @@
-{}:
+{ pkgs }:
 {
-  patches = [ ./fix-folder-registry.patch ];
+  patches = [ ./fix-folder-registry.patch ./fix-llama-binary.patch ];
+  postPatch = ''
+    substituteInPlace llama_binary.py \
+      --replace-fail "@llama-cli@" "${pkgs.llama-cpp}/bin/llama-cli"
+  '';
 }

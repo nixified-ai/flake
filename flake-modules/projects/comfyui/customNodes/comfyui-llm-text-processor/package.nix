273b86ae517291b2c47938d5aaf670024d6c7fce
Adrian Gierakowski <agierakowski@gmail.com>
2026-07-07 16:57:28 +0200

    chore: fix formatting after nix fmt

--- flake-modules/projects/comfyui/customNodes/comfyui-llm-text-processor/package.nix
+++ flake-modules/projects/comfyui/customNodes/comfyui-llm-text-processor/package.nix
@@ -1,6 +1,9 @@
 { pkgs }:
 {
-  patches = [ ./fix-folder-registry.patch ./fix-llama-binary.patch ];
+  patches = [
+    ./fix-folder-registry.patch
+    ./fix-llama-binary.patch
+  ];
   postPatch = ''
     substituteInPlace llama_binary.py \
       --replace-fail "@llama-cli@" "${pkgs.llama-cpp}/bin/llama-cli"

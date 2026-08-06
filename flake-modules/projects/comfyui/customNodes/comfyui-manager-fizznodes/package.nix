72d53048b63a5deee50b9fd844c1c95e66478392
Adrian Gierakowski <agierakowski@gmail.com>
2026-08-06 17:32:34 +0200

    fix: more custom node loading issues

--- flake-modules/projects/comfyui/customNodes/comfyui-manager-fizznodes/package.nix
+++ flake-modules/projects/comfyui/customNodes/comfyui-manager-fizznodes/package.nix
@@ -5,6 +5,8 @@
 {
   postPatch = ''
     substituteInPlace __init__.py \
+      --replace-warn "os.makedirs(extentions_folder)" "pass" \
+      --replace-warn "os.makedirs(extentions_folder, exist_ok=True)" "pass" \
       --replace-fail "result = filecmp.dircmp(javascript_folder, extentions_folder)" "result = None" \
       --replace-fail "if result.left_only or result.diff_files:" "if False:"
   '';
